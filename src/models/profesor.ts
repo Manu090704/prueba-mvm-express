5e486e7744005f15df7be4a25dc4aad6cedb1873
Manu <A01711581@tec.mx>
2024-11-14 18:31:17 -0600

    entrega final

--- src/models/profesor.ts
+++ src/models/profesor.ts
@@ -30,11 +30,11 @@ export const findAllProfesors = async (
 };
 
 export const insertProfesor = async (profesor: Profesor): Promise<Profesor> => {
-  const { first_name, last_name, department, email, address, phone } = profesor;
+  const { first_name, last_name, department, email, phone } = profesor;
   const [result] = await pool.query<ResultSetHeader>(
-    `INSERT INTO teachers (first_name, last_name, department, email, address, phone) 
-     VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-    [first_name, last_name, department, email, address, phone],
+    `INSERT INTO teachers (first_name, last_name, department, email, phone) 
+     VALUES (?, ?, ?, ?, ?)`,
+    [first_name, last_name, department, email, phone],
   );
   const { insertId } = result;
   return { id: insertId, ...profesor };
@@ -44,17 +44,16 @@ export const updateProfesor = async (
   id: number,
   profesor: Profesor,
 ): Promise<Profesor> => {
-  const { first_name, last_name, department, email, address, phone } = profesor;
+  const { first_name, last_name, department, email, phone } = profesor;
   await pool.query<ResultSetHeader>(
     `UPDATE teachers
      SET first_name = ?, 
          last_name = ?, 
          department = ?, 
-         email = ?, 
-         address = ?, 
-         phone = ?, 
+         email = ?,  
+         phone = ?   
      WHERE id = ?;`,
-    [first_name, last_name, department, email, address, phone, id],
+    [first_name, last_name, department, email, phone, id],
   );
 
   return { id, ...profesor };
